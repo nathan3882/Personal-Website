33488cc3c4805f67d4c0603c595223d15878e566
nallanson <nallanson@tucasi.com>
2019-12-03 14:23:08 +0000

    Type safety in LocationService#goHome

--- src/app/services/location/location.service.ts
+++ src/app/services/location/location.service.ts
@@ -1,43 +1,44 @@
 import {Injectable} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {Router} from '@angular/router';
-import {ProjectsService} from "../projects/projects.service";
-import {PersonalProject} from "../../models/PersonalProject";
+import {PersonalProject} from '../../models/PersonalProject';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class LocationService {
 
-    queryParamSubscription: Subscription;
-    private _fromProject: string;
-    router: Router;
+  queryParamSubscription: Subscription;
+  private _fromProject: string;
+  router: Router;
 
-    constructor() {
-    }
+  constructor() {
+  }
 
-    public fromProject(): string {
-        return this._fromProject === undefined ? '' : this._fromProject;
-    }
+  public fromProject(): string {
+    return this._fromProject === undefined ? '' : this._fromProject;
+  }
 
-    public setFromProject(fromProject: string) {
-        this._fromProject = fromProject;
-    }
+  public setFromProject(fromProject: string) {
+    this._fromProject = fromProject;
+  }
 
-    public redirectToProject(projectName: string): Promise<boolean> {
-        return this.router.navigateByUrl('/project/' + projectName);
-    }
+  public redirectToProject(projectName: string): Promise<boolean> {
+    return this.router.navigateByUrl('/project/' + projectName);
+  }
 
-    public redirectToLink(link: string, newTab: boolean): void {
-        window.open(link, "_blank");
-    }
+  public redirectToLink(link: string, newTab: boolean): void {
+    window.open(link, '_blank');
+  }
 
-    goHome(activeProject: PersonalProject) {
-        if (activeProject !== undefined) {
-            let fromThisProject = activeProject.displayName;
-            this.router.navigate(['/'], {queryParams: {fromProject: fromThisProject}});
-        } else {
-            this.router.navigate(['/']);
-        }
+  goHome(fromThisProject: PersonalProject | undefined) {
+    if (fromThisProject !== undefined) {
+      this.router.navigate(['/'], {
+        queryParams:
+          {fromProject: fromThisProject.displayName}
+      });
+    } else {
+      this.router.navigate(['/']);
     }
+  }
 }
